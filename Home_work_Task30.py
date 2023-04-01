6d2b9c53a35199a3283a8207d67d240c48e3d800
Galka <hgalala@mail.ru>
2023-04-01 09:50:59 +0700

    Create file with home work Task30

---
+++ Home_work_Task30.py
@@ -0,0 +1,14 @@
+# Задача 30:  Заполните массив элементами арифметической прогрессии. Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+numberFirst=int(input("Введите первый элемент прогрессии: "))
+numberDifference=int(input("Введите разность: "))
+numberSize=int(input("Введите количество элементов прогрессии: "))
+list=[numberFirst+(i-1)*numberDifference for i in range(1, numberSize+1)]
+print(list)
+
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# for i in range(n):
+# print(a1 + i * d)
